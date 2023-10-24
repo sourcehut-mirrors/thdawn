1ef33dcce152772ff66848202b56811c92885d20
Vincent Lee <vincent@vincent-lee.net>
2023-10-24 10:39:02 -0700

    Checkpoint

--- main.lisp
+++ main.lisp
@@ -20,7 +20,55 @@
 ;; Eventually we should move these out to other files and figure out how to load them
 ;; probably from each other.
 
-;; Bullet and enemy system
+;; Bullet system. Uses SOA/separate dense typed arrays for performance
+(defconstant NUM-BULLETS 512)
+(defvar bullet-types
+  (make-array NUM-BULLETS :element-type 'keyword :initial-element :none)
+  "bullet types, :none means this slot is empty and represents no bullet")
+(defvar bullet-colors
+  (make-array NUM-BULLETS :element-type 'keyword :initial-element :white)
+  "bullet colors")
+(defvar bullet-xs
+  (make-array NUM-BULLETS :element-type 'float :initial-element 0)
+  "bullet x positions")
+(defvar bullet-ys
+  (make-array NUM-BULLETS :element-type 'float :initial-element 0)
+  "bullet y positions")
+(defvar bullet-xvs
+  (make-array NUM-BULLETS :element-type 'float :initial-element 0)
+  "bullet x velocities")
+(defvar bullet-yvs
+  (make-array NUM-BULLETS :element-type 'float :initial-element 0)
+  "bullet y velocities")
+(defvar bullet-control
+  (make-array NUM-BULLETS :element-type '(or null function) :initial-element NIL)
+  "Control function guiding this bullet's movement, called every frame.")
+(defvar bullet-extras
+  (let ((result (make-array NUM-BULLETS)))
+	(dotimes (i NUM-BULLETS)
+	  (setf (aref result i) (make-hash-table :size 4)))
+	result)
+  "Auxiliary hashtable of data any bullets need to store. If something's very commonly used across many bullets, it should get its own array.")
+
+;; Common bullet control functions
+(defun bullet-control-linear (id)
+  "A simple control function that advances the bullet's position according to its (fixed) velocity"
+  (incf (aref bullet-xs id) (aref bullet-xvs id))
+  (incf (aref bullet-ys id) (aref bullet-yvs id)))
+
+(defun bullet-control-accelerate (id)
+  "Simple control function that advances the bullet's position according to its velocity, and applies an acceleration factor to its velocity
+   Requires keys :xa and :ya to be present in the extra data hashtable, or else no acceleration is applied."
+  ;; todo: acceleration common enough to be its own array instead of in extradata?
+  (incf (aref bullet-xs id) (aref bullet-xvs id))
+  (incf (aref bullet-ys id) (aref bullet-yvs id))
+  (let ((data (aref bullet-extras id)))
+	(incf (aref bullet-xvs id) (get :xa data 0))
+	(incf (aref bullet-yvs id) (get :ya data 0))))
+
+;; TODO: Others common control functions we might want (acceleration, deceleration, etc.)
+
+(defconstant NUM-ENM 256)
 
 ;; Boss management
 
@@ -28,7 +76,19 @@
 
 ;; sfx and music
 
-;; resource loading
+;; resources
+(defvar ojamajo-carnival NIL)
+(defvar hud-texture NIL)
+
+(defun load-resources ()
+  ;(setf ojamajo-carnival (load-music-stream "bgm/ojamajo_carnival.wav"))
+  ;(setf hud-texture (load-texture "ryannlib_v1.02/data_assets/THlib/UI/ui_bg.png"))
+  )
+
+(defun unload-resources ()
+  ;(unload-music-stream ojamajo-carnival)
+  ;(unload-texture hud-texture)
+  )
 
 (defcoroutine test-coro (param)
   (yield)
@@ -42,17 +102,18 @@
 	(funcall coro-test "param")
 	(funcall coro-test "param")
 	(funcall coro-test "param"))
+  (load-resources)
   (with-window
 	  (640 480 "thdawn")
 	(set-target-fps 60)
 	(set-exit-key 0)
-	(let ((hud-texture (load-texture "ryannlib_v1.02/data_assets/THlib/UI/ui_bg.png")))
-	  (loop
-		(if (window-should-close)
-			(return))
-		(with-drawing
+	;(play-music-stream ojamajo-carnival)
+	(loop
+	  (if (window-should-close)
+		  (return))
+	  (with-drawing
 		  (clear-background bgcolor)
-		  (draw-texture hud-texture 0 0 :raywhite)
-		  (draw-fps 10 10))))))
+		;(draw-texture hud-texture 0 0 :raywhite)
+		(draw-fps 10 10)))))
 
 (main)
